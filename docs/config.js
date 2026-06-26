7e91371346b46d7e1997a439fe4825c0d02ead0f
Suyyash_Ar <114338825+Suyyash-aroraa@users.noreply.github.com>
2026-06-26 16:06:20 +0530

    Update API_URL to new production endpoint

--- docs/config.js
+++ docs/config.js
@@ -1,3 +1,3 @@
 // Update this URL after deploying to Render
-const API_URL = "https://unwomanly-shame-pastime.ngrok-free.dev";
+const API_URL = "https://timetable-6s8q.onrender.com/";
 const GITHUB_RAW_URL = "https://raw.githubusercontent.com/Suyyash-aroraa/Timetable/main/docs/status.json";
